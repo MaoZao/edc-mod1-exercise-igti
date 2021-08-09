9289785d166e85d53d0715d6fefcb49c7dc147a5
MaoZao <salomao.3@gmail.com>
2021-08-09 16:30:02 -0300

    backend tf s3

---
+++ iac/s3.tf
@@ -0,0 +1,20 @@
+#HCL - Hashicorp Configuration Language
+# Linguagem declarativa
+
+resource "aws_s3_bucket" "dl" {
+  bucket = "datalake-psalomao-igti-tf"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    IES   = "IGTI"
+    CURSO = "EDC"
+  }
+}
+
